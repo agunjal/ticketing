7914b66badb8de7e81c2278c465dc12f69cdfe96
agunjal <agunjal@entrata.com>
2022-04-02 21:17:05 +0530

    console service started

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { app } from "./app";
 
 const start = async () => {
 
+  console.log('Starting up the service...');
+  
   if(!process.env.JWT_KEY) {
     throw new Error('JWT_key must be defined');
   }
